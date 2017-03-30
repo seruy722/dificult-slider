bc23fd8383ef25476c9fd5680057961cd7a24a99
sergey <seruy722@gmail.com>
2017-03-30 15:44:37 +0300

    add btnRigth

--- js/slider.js
+++ js/slider.js
@@ -1,2 +1,46 @@
-'use strict';
+"use strict";
+
+let btnLeft = document.getElementById("left");
+let btnRight = document.getElementById("right");
+let pagination = document.querySelectorAll(".pagination>li");
+const img = ["img/img1.jpg", "img/img2.jpg", "img/img3.jpg"];
+let carousel = document.getElementById("carousel");
+let slideIndex;
+let prevInd;
+let slideWidth = 748;
+let carouselShift = 748;
+let slideCounter = 0;
+let numberOfSlides = carousel.children.length;
+let counter = 0;
+
+btnRight.onclick = () => {
+  let prevInd;
+  counter++;
+  slideCounter++;
+  slideIndex = counter % numberOfSlides;
+  let active = document.querySelector(".active");
+  if (slideCounter >= numberOfSlides) {
+    slideIndex === 0
+      ? (prevInd = numberOfSlides - 1)
+      : (prevInd = slideIndex - 1);
+    carousel.lastElementChild.previousElementSibling.innerHTML = `<img src="${img[prevInd]}">`;
+    carousel.lastElementChild.innerHTML = `<img src="${img[slideIndex]}">`;
+    carousel.style.transform = `translateX(-${carouselShift - slideWidth * 2}px)`;
+    carousel.style.transitionDuration = "";
+
+    setTimeout(
+      function() {
+        carousel.style.transform = `translateX(-${carouselShift - slideWidth}px)`;
+        carousel.style.transitionDuration = ".2s";
+      },
+      0
+    );
+    slideCounter--;
+    return;
+  } else {
+    carousel.style.transform = `translateX(-${carouselShift}px)`;
+    carousel.style.transitionDuration = ".2s";
+    carouselShift += slideWidth;
+  }
+}
 
